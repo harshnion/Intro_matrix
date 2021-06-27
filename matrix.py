7983a5db2ab779d6d11848d5348c9a5511f3823a
harshnion <86431354+harshnion@users.noreply.github.com>
2021-06-27 23:33:50 +0530

    4 commit

---
+++ matrix.py
@@ -0,0 +1,13 @@
+#today we learn about matrix in python 
+
+#It is very easy concept , we will do this with help of nested lists
+
+#Let's take a nested list 
+m = [[1,2,3],[4,5,6]]
+
+#Here, we run loop to print each element of our matrix
+for row in m:
+  for col in row:
+    print(col)
+
+# By running this code we get each element of matrix as output
